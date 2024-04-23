2e54972e614ab7660cd5aa96b67f063b7a2eb1a9
mnmr1996 <mnmr1996develop@gmail.com>
2024-04-23 17:36:12 -0400

    adding in the backend

---
+++ BackEnd - Spring/laughlounge/src/main/kotlin/com/michaelrichards/laughlounge/LaughloungeApplication.kt
@@ -0,0 +1,11 @@
+package com.michaelrichards.laughlounge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LaughloungeApplication
+
+fun main(args: Array<String>) {
+	runApplication<LaughloungeApplication>(*args)
+}
---
+++ BackEnd - Spring/laughlounge/src/test/kotlin/com/michaelrichards/laughlounge/LaughloungeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.michaelrichards.laughlounge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LaughloungeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
